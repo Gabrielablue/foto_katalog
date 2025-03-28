6fefee81e2be8ba399993a7bf3b39683ad925fd7
GabrielaSevcikova <gabikasevcikova9@gmail.com>
2025-03-28 13:16:07 +0100

    Add files via upload

---
+++ parts/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2025 <a href="#">SnapX</a> Photo Contest Co., Ltd. All rights reserved. 
+          
+          Design: <a title="CSS Templates" rel="sponsored" href="https://templatemo.com/page/1" target="_blank">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">
+                            <img src="assets/images/logo.png" alt="SnapX Photography Template">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="index.html" class="active">Home</a></li>
+                            <li class="has-sub">
+                                <a href="javascript:void(0)">Photos &amp; Videos</a>
+                                <ul class="sub-menu">
+                                    <li><a href="contests.html">Contests</a></li>
+                                    <li><a href="contest-details.html">Single Contest</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="categories.html">Categories</a></li>
+                            <li><a href="users.html">Users</a></li>
+                        </ul>   
+                        <div class="border-button">
+                          <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
+                        </div>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+      </header>
+</body>
+</html>
